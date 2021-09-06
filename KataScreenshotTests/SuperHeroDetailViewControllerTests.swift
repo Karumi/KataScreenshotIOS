464615cef6ae51962b02d95bd6fa92221fc64938
Antonio Candinho <antonio@gokarumi.com>
2021-09-06 13:21:15 +0200

    Update to XCode 12

--- KataScreenshotTests/SuperHeroDetailViewControllerTests.swift
+++ KataScreenshotTests/SuperHeroDetailViewControllerTests.swift
@@ -1,21 +1,12 @@
-//
-//  SuperHeroDetailViewControllerTests.swift
-//  KataSuperHeroes
-//
-//  Created by Sergio Gutiérrez on 22/12/16.
-//  Copyright © 2016 GoKarumi. All rights reserved.
-//
-
-import UIKit
 @testable import KataScreenshot
+import UIKit
 
 class SuperHeroDetailViewControllerTests: ScreenshotTest {
-
-    fileprivate let repository = MockSuperHeroesRepository()
+    private let repository = MockSuperHeroesRepository()
 
     func testShowsSuperHeroWithNoBadge() {
         let superHero = givenASuperHero(isAvenger: false)
-        
+
         let viewController = getSuperHeroDetailViewController(superHero.name)
 
         verify(viewController: viewController)
@@ -28,14 +19,16 @@ class SuperHeroDetailViewControllerTests: ScreenshotTest {
 
         verify(viewController: viewController)
     }
+}
 
+private extension SuperHeroDetailViewControllerTests {
     func givenASuperHero(isAvenger: Bool) -> SuperHero {
         let superHero = SuperHeroMother.givenASuperHero(isAvenger: isAvenger)
         repository.superHeroes = [superHero]
         return superHero
     }
 
-    fileprivate func getSuperHeroDetailViewController(_ superHeroName: String) -> UIViewController {
+    func getSuperHeroDetailViewController(_ superHeroName: String) -> UIViewController {
         let superHeroDetailViewController = ServiceLocator()
             .provideSuperHeroDetailViewController(superHeroName) as! SuperHeroDetailViewController
         superHeroDetailViewController.presenter = SuperHeroDetailPresenter(
--- KataScreenshotTests/SuperHeroMother.swift
+++ KataScreenshotTests/SuperHeroMother.swift
@@ -1,12 +1,5 @@
-//
-//  SuperHeroMother.swift
-//  KataScreenshot
-//
-//  Created by Sergio Gutiérrez on 09/01/17.
-//  Copyright © 2017 Karumi. All rights reserved.
-//
-
 import Foundation
+
 @testable import KataScreenshot
 
 class SuperHeroMother {
